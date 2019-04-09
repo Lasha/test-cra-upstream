a1af8a4cbc887b805d7cf89db0fa74613eab17ba
Lasha Krikheli <lasha84@gmail.com>
2019-04-09 00:19:49 -0400

    8 from CLI

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Today! CI test 7.
+            Learn React Today! CI test 8 direct from CLI.
           </a>
         </header>
       </div>
