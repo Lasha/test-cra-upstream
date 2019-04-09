3f4d09f219a623f51a8099673dae4df11fe041b5
Lasha Krikheli <lasha84@gmail.com>
2019-04-09 00:16:01 -0400

    version 7

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Today! CI test 6 via PR.
+            Learn React Today! CI test 7.
           </a>
         </header>
       </div>
